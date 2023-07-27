ff59d587da30b9c5fb0e76b32a7c6107bdd7a369
Rafael Vinicius da Silva Basilio <108074472+RafaelViniciusdaSilvaBasilio@users.noreply.github.com>
2023-07-26 22:30:29 -0300

    feedback
    alteração no código

---
+++ 1546-feedback/resultado.py
@@ -0,0 +1,7 @@
+membros = ['Rolien','Naej', 'Elehcim','Odranoel']
+N = int(input())
+for i in range(N):
+    X = int(input())
+    for i in range(X):
+        feedback = int(input())
+        print(membros[feedback-1])
